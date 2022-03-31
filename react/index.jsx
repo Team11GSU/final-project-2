63e5ba564d14ab73bf944670a84dd3d90f101744
Dileep Reddy <dileepreddyp7@gmail.com>
2022-03-31 13:37:35 -0400

    fixed starter code

--- react/index.jsx
+++ react/index.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { grommet, Grommet } from 'grommet';
 
 // entry point for app and Parcel
 const app = document.getElementById('react');
 ReactDOM.render(
-    <App/>,
+    <Grommet theme={grommet}>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App />} />
+            </Routes>
+        </BrowserRouter>
+    </Grommet>,
     app,
 );
