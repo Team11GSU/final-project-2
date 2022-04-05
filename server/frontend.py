2bdbb06ceba46aa0b3a0e59a1c346b6b65cd3fc7
Dileep Reddy <dileepreddyp7@gmail.com>
2022-04-04 20:44:21 -0400

    refactored Google login flow

--- server/frontend.py
+++ server/frontend.py
@@ -3,6 +3,7 @@ from flask import Blueprint, render_template
 frontend = Blueprint("frontend", __name__, template_folder="templates")
 
 @frontend.route("/")
+@frontend.route("/loginform")
 def index():
     "index"
     return render_template("index.html")
---
+++ server/google_endpoint.py
@@ -0,0 +1,44 @@
+from flask_dance.contrib.google import make_google_blueprint
+from flask_dance.consumer.storage.sqla import SQLAlchemyStorage
+from flask_login import current_user, login_user
+from flask_dance.consumer import oauth_authorized
+from server.models import db, OAuth, User
+
+from os import getenv
+google_blueprint = make_google_blueprint(
+    client_id=getenv("CLIENT_ID"),
+    client_secret=getenv("CLIENT_SECRET"),
+    scope=[
+        "https://www.googleapis.com/auth/userinfo.email",
+        "https://www.googleapis.com/auth/userinfo.profile",
+        "openid",
+    ],
+    offline=True,
+    reprompt_consent=True,
+    storage=SQLAlchemyStorage(OAuth, db.session, user=current_user),
+)
+
+
+@oauth_authorized.connect_via(google_blueprint)
+def google_logged_in(blueprint, token):
+    resp = blueprint.session.get("/oauth2/v2/userinfo")
+    user_info = resp.json()
+    user_id = str(user_info["id"])
+    oauth = OAuth.query.filter_by(
+        provider=blueprint.name, provider_user_id=user_id
+    ).first()
+    if not oauth:
+        oauth = OAuth(provider=blueprint.name, provider_user_id=user_id, token=token)
+    else:
+        oauth.token = token
+        db.session.add(oauth)
+        db.session.commit()
+        login_user(oauth.user)
+    if not oauth.user:
+        user = User(email=user_info["email"], name=user_info["name"])
+        oauth.user = user
+        db.session.add_all([user, oauth])
+        db.session.commit()
+        login_user(user)
+
+    return False
