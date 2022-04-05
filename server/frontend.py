43fdbca6bb326e3ad406379358434f2a80cda721
Dileep Reddy <dileepreddyp7@gmail.com>
2022-04-04 21:35:42 -0400

    pylintrc

--- server/frontend.py
+++ server/frontend.py
@@ -4,6 +4,10 @@ frontend = Blueprint("frontend", __name__, template_folder="templates")
 
 @frontend.route("/")
 @frontend.route("/loginform")
+@frontend.route("/calendar")
+@frontend.route("/todo")
+@frontend.route("/chat")
+@frontend.route("/files")
 def index():
     "index"
     return render_template("index.html")
