c9fbdacf0b96175c9ee0b18ebed79ad385573a6e
Nahid Hossain <mdnhossain11@gmail.com>
2022-04-08 17:50:54 -0400

    adding changes to gmail

--- server/frontend.py
+++ server/frontend.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template
 
 frontend = Blueprint("frontend", __name__, template_folder="templates")
 
+
 @frontend.route("/")
 @frontend.route("/loginform")
 @frontend.route("/project/<projectID>/")
@@ -9,6 +10,7 @@ frontend = Blueprint("frontend", __name__, template_folder="templates")
 @frontend.route("/project/<projectID>/todo")
 @frontend.route("/project/<projectID>/chat")
 @frontend.route("/project/<projectID>/files")
+@frontend.route("/project/<projectID>/email")
 def index(projectID=1):
     "index"
     return render_template("index.html")
