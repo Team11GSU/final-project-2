7fe251fefd72b40d2a8516a1d01eb3d00b22185b
sjiang8 <2000901sj@gmail.com>
2022-04-08 00:26:50 -0400

    initial commit

---
+++ react/components/TodoContainer.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import TodoTask from './TodoItem';
+import TodoForms from './TodoForms';
+import { useParams } from 'react-router-dom';
+
+const params = useParams();
+
+const TodoItem = [
+    {
+        id: 1,
+        TaskName: 'my first task',
+        complete: true,
+        project: params.projectID,
+    },
+]
+
+const TodoContainer = () => {
+    const [todoList, setTodoList] = useState(TodoItem);
+    const handleAddItem = (userInput) => {
+        const item = [...todoList, userInput];
+        setTodoList(item);
+    }
+
+    const handleClick = (id) => {
+        const item = todoList.map(todo => {
+            if (todo.id == id) {
+                return { ...todo, complete: !todo.complete }
+            }
+            return todo;
+        })
+        setTodoList(item)
+    }
+
+    const handleDelete = (id) => {
+        let item = todoList.filter(todo =>
+            todo.id !== id
+        );
+        setTodoList(item)
+
+    }
+
+    return (
+        <div>
+            {todoList.length > 0 ? todoList.map(
+                (todo) => <TodoTask todo={todo} key={todo.id} deleteTodo={handleDelete}handleChange={handleClick} />) : <p>your task is clean</p>}
+
+            <TodoForms addItem={handleAddItem} />
+
+        </div>
+    )
+}
+
+export default TodoContainer
